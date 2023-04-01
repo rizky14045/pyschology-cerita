83e2575751090f2952826133753a6c23e9b940b5
rizky14045 <mrizkysaputra17@gmail.com>
2023-04-01 16:34:15 +0700

    add price admin

---
+++ resources/views/admin/price/update.blade.php
@@ -0,0 +1,54 @@
+@extends('admin.layout.app')
+@section('styles')
+@stop
+@section('content')
+<div class="page-title-box">
+    <div class="row align-items-center">
+            <div class="col-md-8">
+                <h6 class="page-title">Price</h6>
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="{{url('admin')}}">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{route('admin.price.index')}}">Price</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Create</li>
+                </ol>
+            </div>
+        </div> 
+    </div>
+</div>
+<div class="card">
+    <div class="card-body">
+        <form action="{{route('admin.price.update',['id'=>$price->id])}}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PATCH')
+            <div class="row pb-3">
+                <div class="form-group col-12">
+                    <label>Title</label>
+                    <input type="text" class="form-control" name="title" required value="{{$price->title}}">
+                    @if($errors->has('title'))
+                    <div class="error">{{ $errors->first('title') }}</div>
+                @endif
+                </div>
+            </div>
+            <div class="row pb-3">
+                <div class="form-group col-12">
+                    <label>Price</label>
+                    <input type="text" class="form-control" name="price" required value="{{$price->price}}">
+                    @if($errors->has('price'))
+                    <div class="error">{{ $errors->first('price') }}</div>
+                @endif
+                </div>
+            </div>
+            <div class="form-group" align="right">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="reset" class="btn btn-success">Reset</button>
+                <a href="{{route('admin.price.index')}}" class="btn btn-danger">Back</a>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+@stop
+@section('scripts')
+
+@stop
