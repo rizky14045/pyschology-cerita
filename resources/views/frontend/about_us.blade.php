c38d3d38fd9f8b6eb58b83cca0f546cf78180f41
rizky14045 <mrizkysaputra17@gmail.com>
2023-04-06 14:56:50 +0700

    add asset

--- resources/views/frontend/about_us.blade.php
+++ resources/views/frontend/about_us.blade.php
@@ -23,21 +23,13 @@
         <div class="container-fluid p-0">
             <div class="row m-0">
                 <div class="col-lg-6 col-md-12 p-0">
-                    <img src="{{ asset('assets/vendor/img/album1.jpg') }}" alt="image">
+                    <img src="{{ asset('assets/vendor/img/album31.jpg') }}" alt="image">
                 </div>
                 <div class="col-lg-6 col-md-12 p-0">
                     <div class="about-content">
                         <span>Tentang Kami</span>
-                        <h2>Short Story About Fovia Clinic Since 1999</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
-                        <ul>
-                            <li><i class="icofont-tick-mark"></i> Scientific Skills For getting a better result</li>
-                            <li><i class="icofont-tick-mark"></i> Communication Skills to getting in touch</li>
-                            <li><i class="icofont-tick-mark"></i> A Career Overview opportunity Available</li>
-                            <li><i class="icofont-tick-mark"></i> A good Work Environment For work</li>
-                            <li><i class="icofont-tick-mark"></i> Scientific Skills For getting a better result</li>
-                            <li><i class="icofont-tick-mark"></i> Communication Skills to getting in touch</li>
-                        </ul>
+                        <p align="justify">Saatnya kamu memiliki akses pada psikolog profesional yang sesuai dengan topik permasalahan yang sedang kamu alami. 
+                            Bagikan Ceritamu memberikan kebebasan kamu bercerita apapun masalahnya dan bertemu ahlinya untuk mendapat jawaban-jawaban terbaik atas keluh kesahmu tanpa khawatir keamanan, karena setiap informasi data yang kami terima akan kami jaga kerahasiaannya. Para Psikolog yang tergabung di bagikanceritamu adalah mereka yang memiliki pengalaman dan wawasan sesuai topik permasalahan. Ketika kamu datang pada kami dengan masalah yang sedang dialami, psikolog kami akan melakukan observasi yang kemudian melakukan pendalaman untuk memahami masalahmu. Sikap yang terbuka, ramah, pendengar dan tutur kata yang baik menjadi prioritas dalam memberikan layanan konseling. </p>
                     </div>
                 </div>
             </div>
@@ -53,8 +45,8 @@
                         <div class="icon">
                             <i class="icofont-tick-mark"></i>
                         </div>
-                        <h3>Our Mission</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.</p>
+                        <h3>Fokus</h3>
+                        <p>Kami memberikan perhatian penuh pada keluhan masalah yang kamu sampaikan. Fokus kami memberikan pelayanan senyaman mungkin.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-12">
@@ -62,8 +54,8 @@
                         <div class="icon">
                             <i class="icofont-tick-mark"></i>
                         </div>
-                        <h3>Our Planning</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.</p>
+                        <h3>Ramah</h3>
+                        <p>Sikap yang ramah dan tutur kata yang baik menjadi hal utama yang diberikan oleh para psikolog kami.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-12">
@@ -71,53 +63,68 @@
                         <div class="icon">
                             <i class="icofont-tick-mark"></i>
                         </div>
-                        <h3>Our Vision</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.</p>
+                        <h3>Kerahasiaan Terjaga</h3>
+                        <p>Kami sangat berkomitmen untuk menjaga kerahasiaan informasi yang kamu berikan</p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-
-
-    <section class="our-mission-area ptb-100 pt-0">
-        <div class="container-fluid p-0">
-            <div class="row m-0">
-                <div class="col-lg-6 col-md-12 p-0">
-                    <div class="our-mission-content">
-                        <span class="sub-title">Our Mission &amp; Vision</span>
-                        <h2>Better Information, Better Health</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <ul>
-                            <li>
-                                <div class="icon">
-                                    <i class="icofont-doctor"></i>
-                                </div>
-                                <span>Professional Staff</span> Lorem ipsum dolor sit amet sit, consectetur adipiscing elit.
-                            </li>
-                            <li>
-                                <div class="icon">
-                                    <i class="icofont-kid"></i>
-                                </div>
-                                <span>Newborn Care</span> Lorem ipsum dolor sit amet sit, consectetur adipiscing elit.
-                            </li>
-                            <li>
-                                <div class="icon">
-                                    <i class="icofont-laboratory"></i>
-                                </div>
-                                <span>Sufficient Lab Tests</span> Lorem ipsum dolor sit amet sit, consectetur adipiscing elit.
-                            </li>
-                            <li>
-                                <div class="icon">
-                                    <i class="icofont-tooth"></i>
-                                </div>
-                                <span>Tooth Extraction</span> Lorem ipsum dolor sit amet sit, consectetur adipiscing elit.
-                            </li>
-                        </ul>
+    <section class="why-choose section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="choose-left">
+                        <div class="row">
+                            <div class="col-lg-5">
+                                <ul class="list">
+                                    <li>
+                                        <h4>Visi</h4>
+                                    </li>
+                                    Menumbuhkan, merawat dan menjaga rasa kepercayaan diri pada setiap individu bahwa
+                                    dirinya adalah anugerah terbaik dari Tuhan
+                                </ul>
+                            </div>
+                            <div class="col-lg-7">
+                                <ul class="list">
+                                    <li>
+                                        <h4>Misi</h4>
+                                    </li>
+                                    <li>
+                                        <div class="row">
+                                            <div class="col-1">
+                                                <i class="fa fa-caret-right"></i>
+                                            </div>
+                                            <div class="col-11">
+                                                Memberikan akses layanan psikolog profesional yang
+                                                mudah dan terjangkau bagi semua kalangan.
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="row">
+                                            <div class="col-1">
+                                                <i class="fa fa-caret-right"></i>
+                                            </div>
+                                            <div class="col-11">
+                                                Menjadi teman bercerita masalah kesehatan mental yang memberikan keamanan dan kenyamanan.
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="row">
+                                            <div class="col-1">
+                                                <i class="fa fa-caret-right"></i>
+                                            </div>
+                                            <div class="col-11">
+                                                Menjadi jembatan terjalinnya koneksivitas yang baik antara individu dengan psikolog.
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-6 col-md-12 p-0">
-                    <img src="{{ asset('assets/vendor/img/album3.jpg') }}" alt="image">
                 </div>
             </div>
         </div>
